c9e67f86fe7b0c998db8908f0d2c0de7c8d50cb5
Eliott Frohly <frohlyeliott@gmail.Com>
2024-02-20 10:11:17 -0500

    Programme principal avec 4 fonctions tests

--- src/main.c
+++ src/main.c
@@ -1,8 +1,51 @@
-void tester_tableau1d(void);
-void tester_tableau2d(void);
-void tester_noyau(void);
-void tester_image_gris(void);
+#include "tableau/tableau1d.h"
+#include <stdio.h>
+
+/****************************************************************************************
+*                               DEFINTION DES CONSTANTES                                *
+****************************************************************************************/
+
+/****************************************************************************************
+*                           DECLARATION DES FONCTIONS PRIVEES                           *
+****************************************************************************************/
+
+static void tester_tableau1d(void);
+static void tester_tableau2d(void);
+static void tester_noyau(void);
+static void tester_image_gris(void);
 
 int main() {
+    tester_tableau1d();
+    tester_tableau2d();
+    tester_noyau();
+    tester_image_gris();
     return 0;
+}
+
+/****************************************************************************************
+*                           DEFINTION DES FONCTIONS PRIVEES                            *
+****************************************************************************************/
+
+void tester_tableau1d(void) {
+    printf("*********************************************************\n");
+    printf("*                     TEST TABLEAU1D                    *\n");
+    printf("*********************************************************\n");
+}
+
+void tester_tableau2d(void) {
+    printf("*********************************************************\n");
+    printf("*                     TEST TABLEAU2D                    *\n");
+    printf("*********************************************************\n");
+}
+
+void tester_noyau(void) {
+    printf("*********************************************************\n");
+    printf("*                     TEST NOYAU                        *\n");
+    printf("*********************************************************\n");
+}
+
+void tester_image_gris(void) {
+    printf("*********************************************************\n");
+    printf("*                     TEST IMAGE GRIS                   *\n");
+    printf("*********************************************************\n");
 }
