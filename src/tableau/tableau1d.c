801350a183ac1f93c87233428aa01bb63e85b306
Eliott Frohly <frohlyeliott@gmail.Com>
2024-02-20 11:01:42 -0500

    initialisation et entête des fonctions tableaux1D

--- src/tableau/tableau1d.c
+++ src/tableau/tableau1d.c
@@ -19,10 +19,62 @@
 /****************************************************************************************
 *                           DEFINTION DES FONCTIONS PUBLIQUES                            *
 ****************************************************************************************/
+/*
+NOM DE LA FONCTION :
 
+DESCRIPTION
 
+PARAMÈTRES :
 
+VALEUR DE RETOUR :
+*/
+void afficher_tableau1d(double *tableau, int taille){
 
+}
+/*
+NOM DE LA FONCTION :
+
+DESCRIPTION
+
+PARAMÈTRES :
+
+VALEUR DE RETOUR :
+*/
+double somme_tableau1d(double *tableau, int taille){
+
+}
+
+double produit_tableau1d(double *tableau, int taille,double facteur){
+
+}
+
+double produit_scalaire1D(double *tableau1, double *tableau2, int taille){
+
+}
+/*
+NOM DE LA FONCTION :
+
+DESCRIPTION
+
+PARAMÈTRES :
+
+VALEUR DE RETOUR :
+*/
+double* creer_tableau1d(int taille){
+
+}
+/*
+NOM DE LA FONCTION :
+
+DESCRIPTION
+
+PARAMÈTRES :
+
+VALEUR DE RETOUR :
+*/
+void detruire_tableau1d(double **tableau){
+
+}
 /****************************************************************************************
 *                           DEFINTION DES FONCTIONS PRIVEES                            *
 ****************************************************************************************/
--- src/tableau/tableau1d.h
+++ src/tableau/tableau1d.h
@@ -12,3 +12,10 @@
 /****************************************************************************************
 *                       DECLARATION DES FONCTIONS PUBLIQUES                             *
 ****************************************************************************************/
+
+void afficher_tableau1d(double *tableau, int taille);
+double somme_tableau1d(double *tableau, int taille);
+double produit_tableau1d(double *tableau, int taille,double facteur);
+double produit_scalaire1D(double *tableau1, double *tableau2, int taille);
+double* creer_tableau1d(int taille);
+void detruire_tableau1d(double **tableau);
