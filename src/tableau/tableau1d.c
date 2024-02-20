423add34ff6189d352bd8df2f96d2c57f07c3319
Eliott Frohly <frohlyeliott@gmail.Com>
2024-02-20 11:39:06 -0500

    Fonctions 2 3 6 T1D

--- src/tableau/tableau1d.c
+++ src/tableau/tableau1d.c
@@ -2,7 +2,7 @@
     TABLEAU_1D
 
 *****************************************************************************************/
-
+#include <stdlib.h>
 
 /****************************************************************************************
 *                               DEFINTION DES CONSTANTES                                *
@@ -20,7 +20,7 @@
 *                           DEFINTION DES FONCTIONS PUBLIQUES                            *
 ****************************************************************************************/
 /*
-NOM DE LA FONCTION : afficher tableau
+NOM DE LA FONCTION 1 : afficher tableau
 
 DESCRIPTION
 
@@ -32,7 +32,7 @@ void afficher_tableau1d(double *tableau, int taille){
 
 }
 /*
-NOM DE LA FONCTION : somme_tableau1d
+NOM DE LA FONCTION 2 : somme_tableau1d
 
 DESCRIPTION
 
@@ -41,7 +41,7 @@ PARAMÈTRES : - tableau : Un tableau à une dimension à analyser.
 
 VALEUR DE RETOUR :
 */
-double somme_tableau1d(double *tableau, int taille){
+double somme_tableau1d( double *tableau, int taille){
     double somme = 0;
     for (int i = 0; i < taille; i++) {
         somme += tableau[i];
@@ -49,15 +49,40 @@ double somme_tableau1d(double *tableau, int taille){
     return somme;
 }
 
-double produit_tableau1d(double *tableau, int taille,double facteur){
+/*
+NOM DE LA FONCTION 3 : produit_tableau1d
 
+DESCRIPTION :
+    Cette fonction multiplie chaque élément d'un tableau à une dimension par un facteur.
+
+PARAMÈTRES :
+    - tableau : Un tableau à une dimension à analyser.
+    - taille : taille du tableau d'entrée
+    - facteur : Le facteur par lequel chaque élément du tableau sera multiplié.
+
+VALEUR DE RETOUR :
+    Aucune.
+*/
+void produit_tableau1d(double *tableau, int taille,double facteur){
+    for (int i = 0; i < taille; i++) {
+        tableau[i] *= facteur;
+    }
 }
 
+/*
+NOM DE LA FONCTION 4 : produit_tableau1d
+
+DESCRIPTION
+
+PARAMÈTRES :
+
+VALEUR DE RETOUR :
+*/
 double produit_scalaire1D(double *tableau1, double *tableau2, int taille){
 
 }
 /*
-NOM DE LA FONCTION :
+NOM DE LA FONCTION 5 :
 
 DESCRIPTION
 
@@ -69,16 +94,21 @@ double* creer_tableau1d(int taille){
 
 }
 /*
-NOM DE LA FONCTION :
+NOM DE LA FONCTION 6 : detruire_tableau1d
 
-DESCRIPTION
+DESCRIPTION :
+    Cette fonction libère la mémoire allouée pour un tableau à une dimension
+    et remplace l'adresse du tableau par 0.
 
 PARAMÈTRES :
+    - tableau : Un pointeur vers un tableau à une dimension.
 
 VALEUR DE RETOUR :
+    Aucune.
 */
 void detruire_tableau1d(double **tableau){
-
+    free(*tableau);
+    *tableau = 0;
 }
 /****************************************************************************************
 *                           DEFINTION DES FONCTIONS PRIVEES                            *
